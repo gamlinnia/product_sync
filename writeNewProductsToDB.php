b17f97193c6f86744e922978318f8d6c9a03d9e4
gamlinnia <intppp01>
2015-10-20 21:34:54 +0800

    app('default');

--- writeNewProductsToDB.php
+++ writeNewProductsToDB.php
@@ -6,7 +6,7 @@ $setting = json_decode(file_get_contents('setting.json'), true);
 require_once '../' . $config['magentoDir'] . 'app/Mage.php';
 require_once 'functions.php';
 /* if use admin, then websiteId will get 0 */
-Mage::app();
+Mage::app('default');
 
 $host = $setting['hostName'];
 $param = array(
