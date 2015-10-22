25e230666c224da6448c4dd36a8164242e353e64
Li Liu <Li.L.Liu@newegg.com>
2015-10-22 14:54:52 +0800

    getVideoCollection.php

--- getVideoCollection.php
+++ getVideoCollection.php
@@ -9,24 +9,22 @@ require_once '../' . $config['magentoDir'] . 'app/Mage.php';
 require_once 'functions.php';
 Mage::app();
 
-//$productvideos_collection=Mage::getModel('productvideos/productvideos')->getCollection();
-//
-//$response = array();
-//foreach ($productvideos_collection as $productvideo) {
-//    $response[] = $productvideo->debug();
-//}
-//
-//var_dump($response);
-
-
 $videoGalleryCollection = Mage::getModel("videogallery/videogallery")->getCollection();
+$productvideos_collection=Mage::getModel('productvideos/productvideos')->getCollection();
 $response = array();
 
 foreach ($videoGalleryCollection as $videoGallery) {
-    //var_dump($videoGallery);
-    //die();
-    $response[] = $videoGallery->debug();
-    var_dump($response);
-    die();
+    $tmpArray = $videoGallery->debug();
+
+    foreach ($productvideos_collection as $productvideo) {
+        if($tmpArray["videogallery_id"] == $productvideo->getData("videogallery_id")){
+            $tmpArray["product_id"] = $productvideo->getData("product_id");
+        }
+    }
+    $response[] = $tmpArray;
+
 }
+var_dump($response);
+
+//$response = array();
 
