a5bfaab016ba78ab0ab8bc4894ff473bbf008aa5
Li Liu <Li.L.Liu@newegg.com>
2015-10-22 14:02:22 +0800

    get video collection

---
+++ getVideoCollection.php
@@ -0,0 +1,19 @@
+#!/usr/bin/php -q
+<?php
+
+if (!file_exists('config.json')) {
+    echo 'config.json is not exist.';
+}
+$config = json_decode(file_get_contents('config.json'), true);
+require_once '../' . $config['magentoDir'] . 'app/Mage.php';
+require_once 'functions.php';
+Mage::app();
+
+$productvideos_collection=Mage::getModel('productvideos/productvideos')->getCollection();
+
+$response = array();
+foreach ($productvideos_collection as $productvideo) {
+    $response[] = $productvideo->debug();
+}
+
+var_dump($response);
