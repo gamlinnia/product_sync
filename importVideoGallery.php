b1d4a5ff38bea1233aea9939e955cead79e63ace
th98 <tim.h.huang@newegg.com>
2015-10-23 15:04:41 +0800

    importVideoGallery.php

--- importVideoGallery.php
+++ importVideoGallery.php
@@ -9,21 +9,6 @@ require_once '../' . $config['magentoDir'] . 'app/Mage.php';
 require_once 'functions.php';
 Mage::app();
 
-// Remove unset images, add image to gallery if exists
-$importDir = Mage::getBaseDir('media') . DS . 'import/';
-if (!file_exists($importDir)) {
-    mkdir($importDir);
-}
-$url = 'http://www.bikez.com/pictures/um/2007/dsf-200.jpg';
-$pathInfo = pathinfo($url);     // get array of dirname, basename, extension, filename
-$fileName = getFileNameFromUrl($url);
-if (!$fileName) {
-    die('Can not get file name from url');
-}
-$tmpFile = file_get_contents($url);
-file_put_contents($importDir . $fileName, $tmpFile);
-$filePath = $importDir . $fileName;
-
 $data = array(
     'videogallery_id' => '102',
     'videogallery_category' =>  'Product Video',
@@ -34,11 +19,31 @@ $data = array(
     'created' =>  '2015-10-23'
 );
 
-$videoimage = $fileName;
 $videoname = 'test video';
-$model = Mage::getModel('videogallery/videogallery');
 
-$model -> addImageToVideoGallery($filePath);
+$url = 'http://www.bikez.com/pictures/um/2007/dsf-200.jpg';
+if(isset($data['videogallery_url']) && $data['videogallery_url'] != '') {
+    if(!file_exists(Mage::getBaseDir('media').'/videogallery/'))mkdir(Mage::getBaseDir('media').'/videogallery/',0777);
+    //$img_file = $videourl;
+    $img_file = $url;
+    $img_file=file_get_contents($img_file);
+    $file_loc=Mage::getBaseDir('media').DS."videogallery".DS.'videogallery_'.$videoimage.'.jpg';
+
+    $file_handler=fopen($file_loc,'w');
+
+    if(fwrite($file_handler,$img_file)==false){
+        echo 'error';
+    }
+    fclose($file_handler);
+
+    $newfilename ='videogallery_'.$videoimage.'.jpg';
+    // Upload the image
+    $videoimage = $newfilename;
+
+}
+
+
+$model = Mage::getModel('videogallery/videogallery');
 //$model->setData($data);
 $model->setData($data)->setImage($videoimage)->setName($videoname)->setVideogalleryUrl($data['videogallery_url'])->setVideogalleryCategory($data['videogallery_category']);
 $model -> save();
