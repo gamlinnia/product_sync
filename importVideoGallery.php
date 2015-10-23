dd6e8c0f0d885130f205773a12e873c9f1537454
th98 <tim.h.huang@newegg.com>
2015-10-23 14:48:59 +0800

    importVideopGallery.php

--- importVideoGallery.php
+++ importVideoGallery.php
@@ -9,19 +9,35 @@ require_once '../' . $config['magentoDir'] . 'app/Mage.php';
 require_once 'functions.php';
 Mage::app();
 
+// Remove unset images, add image to gallery if exists
+$importDir = Mage::getBaseDir('media') . DS . 'import/';
+if (!file_exists($importDir)) {
+    mkdir($importDir);
+}
+$url = 'http://www.bikez.com/pictures/um/2007/dsf-200.jpg';
+$pathInfo = pathinfo($url);     // get array of dirname, basename, extension, filename
+$fileName = getFileNameFromUrl($url);
+if (!$fileName) {
+    die('Can not get file name from url');
+}
+$tmpFile = file_get_contents($url);
+file_put_contents($importDir . $fileName, $tmpFile);
+$filePath = $importDir . $fileName;
+
 $data = array(
-    'videogallery_id' => '100',
+    'videogallery_id' => '101',
     'videogallery_category' =>  'Product Video',
     'videogallery_url' =>  'https://www.youtube.com/watch?v=OUI6iM8iPOs',
     'name' =>  'test video',
-    'image' =>  'ideogallery_OUI6iM8iPOs.jpg',
+    'image' =>  'videogallery_OUI6iM8iPOs.jpg',
     'gallery_image' => '',
     'created' =>  '2015-10-23'
 );
 
-$videoimage = 'ideogallery_OUI6iM8iPOs.jpg';
+$videoimage = $filePath;
 $videoname = 'test video';
 $model = Mage::getModel('videogallery/videogallery');
+
 //$model->setData($data);
 $model->setData($data)->setImage($videoimage)->setName($videoname)->setVideogalleryUrl($data['videogallery_url'])->setVideogalleryCategory($data['videogallery_category']);
 $model -> save();
