dbdeb649231a906968800ebc620f27ce1dd6158e
th98 <tim.h.huang@newegg.com>
2015-10-23 14:35:24 +0800

    importVideopGallery.php

--- importVideoGallery.php
+++ importVideoGallery.php
@@ -10,16 +10,17 @@ require_once 'functions.php';
 Mage::app();
 
 $data = array(
+    'videogallery_id' => 100,
     'videogallery_category' =>  'Product Video',
     'videogallery_url' =>  'https://www.youtube.com/watch?v=OUI6iM8iPOs',
     'name' =>  'test video',
     'image' =>  'ideogallery_OUI6iM8iPOs.jpg',
+    'gallery_image' => '',
     'created' =>  '2015-10-23'
 );
 
 $videoimage = 'ideogallery_OUI6iM8iPOs.jpg';
 $videoname = 'test video';
-
 $model = Mage::getModel('videogallery/videogallery');
 $model->setData($data);
 //$model->setImage($videoimage)->setName($videoname)->setVideogalleryUrl($data['videogallery_url'])->setVideogalleryCategory($data['videogallery_category']);
