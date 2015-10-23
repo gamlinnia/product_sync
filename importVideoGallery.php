35f52317d6520f9df176fba59bce787fbab1c5af
th98 <tim.h.huang@newegg.com>
2015-10-23 14:27:17 +0800

    importVideopGallery.php

--- importVideoGallery.php
+++ importVideoGallery.php
@@ -9,5 +9,17 @@ require_once '../' . $config['magentoDir'] . 'app/Mage.php';
 require_once 'functions.php';
 Mage::app();
 
+$data = array(
+    'videogallery_category' =>  'Product Video',
+    'videogallery_url' =>  'https://www.youtube.com/watch?v=OUI6iM8iPOs',
+    'name' =>  'test video',
+    'image' =>  'ideogallery_OUI6iM8iPOs.jpg',
+    'created' =>  '2015-10-23'
+);
+
+$videoimage = 'ideogallery_OUI6iM8iPOs.jpg';
+$videoname = 'test video';
+
 $model = Mage::getModel('videogallery/videogallery');
-$model->setData($data)->setImage($videoimage)->setName($videoname)->setVideogalleryUrl($data['videogallery_url'])->setVideogalleryCategory($data['videogallery_category']);
+$model->setData($data);
+//$model->setImage($videoimage)->setName($videoname)->setVideogalleryUrl($data['videogallery_url'])->setVideogalleryCategory($data['videogallery_category']);
