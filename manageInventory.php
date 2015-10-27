a33a140f9de3ed32f9e9f67c94b55f54f5a6a133
Li Liu <Li.L.Liu@newegg.com>
2015-10-27 15:21:33 +0800

    manage inventory

---
+++ manageInventory.php
@@ -0,0 +1,20 @@
+#!/usr/bin/php -q
+<?php
+
+if (!file_exists('config.json')) {
+    echo 'config.json is not exist.';
+}
+$config = json_decode(file_get_contents('config.json'), true);
+require_once '../' . $config['magentoDir'] . 'app/Mage.php';
+require_once 'functions.php';
+Mage::app();
+
+$productCollection = Mage::getModel('catalog/product')->getCollection();
+
+foreach ($productCollection as $product) {
+    $stockItem = Mage::getModel('cataloginventory/stock_item')->loadByProduct($product);
+    $stockItem->setData('manage_stock', 1);
+    $stockItem->setData('is_in_stock', 1);
+    $stockItem->setData('qty', 100);
+    $stockItem->save();
+}
