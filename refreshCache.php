b745391041c5fc63b165f8d00d898111235a7dca
thisisbook <gamlinnia@hotmail.com>
2015-10-29 23:15:53 +0800

    refreshCache

---
+++ refreshCache.php
@@ -0,0 +1,14 @@
+#!/usr/bin/php -q
+<?php
+
+$config = json_decode(file_get_contents('config.json'), true);
+$setting = json_decode(file_get_contents('setting.json'), true);
+require_once '../' . $config['magentoDir'] . 'app/Mage.php';
+require_once 'functions.php';
+/* if use admin, then websiteId will get 0 */
+Mage::app('default');
+
+foreach (array('block_html','collections','fpc') as $type) {
+    Mage::app()->getCacheInstance()->cleanType($type);
+    Mage::dispatchEvent('adminhtml_cache_refresh_type', array('type' => $type));
+}
