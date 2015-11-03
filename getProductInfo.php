d9093032a20e0fdbe37d217bb66674787b2ce705
th98 <tim.h.huang@newegg.com>
2015-11-03 15:54:14 +0800

    getProductInfo.php

--- getProductInfo.php
+++ getProductInfo.php
@@ -8,4 +8,4 @@ require_once 'functions.php';
 Mage::app();
 
 $product = Mage::getModel('catalog/product')->load(1961);
-var_dump(json_encode($product->getData()));
+echo json_encode($product->getData());
