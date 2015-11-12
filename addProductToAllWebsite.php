2582052d07b21fc3b290f7502f9cbfb9db7f3dec
th98 <tim.h.huang@newegg.com>
2015-11-12 17:00:31 +0800

    addProductToAllWebsite.php

--- addProductToAllWebsite.php
+++ addProductToAllWebsite.php
@@ -12,12 +12,24 @@ var_dump($websiteIds);
 //die();
 
 $productList = Mage::getModel('catalog/product')->getCollection();
+$i=0;
 foreach ($productList as $product){
     echo 'SKU: ' . $product->getSku() . PHP_EOL;
+    echo 'URL Key: ' . $product->getUrlKey() . PHP_EOL;
     if($product->getUrlKey()){
-        echo 'URL Key: ' . $product->getUrlKey() . PHP_EOL;
         $product->setUrlKey(false);
     }
+    else{
+        $url = preg_replace('#[^0-9a-z]+#i', '-', $product->getName());
+        $url = strtolower($url);
+        $product->setUrlKey($url);
+    }
+    echo 'New URL Key: ' . $product->getUrlKey() . PHP_EOL;
     $product->setWebsiteIds($websiteIds);
     $product->save();
+    $i++;
+    if($i==2)
+    {
+        die();
+    }
 }
