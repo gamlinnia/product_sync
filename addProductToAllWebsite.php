743cc4a6e0326494011073b9dee6af81c400668f
th98 <tim.h.huang@newegg.com>
2015-11-13 10:31:19 +0800

    addProductToAllWebsite.php

--- addProductToAllWebsite.php
+++ addProductToAllWebsite.php
@@ -7,52 +7,31 @@ require_once 'functions.php';
 Mage::app('admin');
 
 $websiteIds = getAllWebisteIds();
-//echo 'Website IDs: ';
-//var_dump($websiteIds);
+$productList = Mage::getModel('catalog/product')->getCollection();
 
-$productList = Mage::getModel('catalog/product')
-               ->getCollection();
-//               ->addAttributeToSelect('name')
-//               ->addAttributeToSelect('url_key');
-$totalProductNum = count($productList);
-
-$pageSize = 100;
-for($currentPage=1 ; $currentPage <= ($totalProductNum/$currentPage+1) ; $currentPage++){
-    $productList = Mage::getModel('catalog/product')
-        ->getCollection()
-        ->addAttributeToSelect('name')
-        ->addAttributeToSelect('url_key')
-        ->addAttributeToSelect('website_ids')
-        ->setPageSize($pageSize)
-        ->setCurPage($currentPage);
-
-    foreach ($productList as $product) {
-        //var_dump($product->getData());
-        //die();
+foreach ($productList as $each) {
+    $product = Mage::getModel('catalog/product')->load($each->getId());
+    echo 'Current Page: ' . $currentPage . PHP_EOL;
+    echo 'SKU: ' . $product->getSku() . PHP_EOL;
+    echo 'URL Key: ' . $product->getUrlKey() . PHP_EOL;
+    $url_key = $product->getUrlKey();
+    if (!empty($url_key)) {
+        $product->setUrlKey(false);
+    } else {
+        $url = preg_replace('/[^0-9a-z]+/i', '-', $product->getName());
+        $url = strtolower($url);
+        $product->setUrlKey($url);
+        echo 'New URL Key: ' . $url . PHP_EOL;
+    }
+    $oldWebsiteIds = $product->getWebsiteIds();
+    if ( $oldWebsiteIds !== $websiteIds)
+    {
+        $product->setWebsiteIds($websiteIds);
         try {
-            echo 'Current Page: ' . $currentPage . PHP_EOL;
-            echo 'SKU: ' . $product->getSku() . PHP_EOL;
-            echo 'URL Key: ' . $product->getUrlKey() . PHP_EOL;
-            $url_key = $product->getUrlKey();
-            if (!empty($url_key)) {
-                $product->setUrlKey(false);
-            } else {
-                $url = preg_replace('/[^0-9a-z]+/i', '-', $product->getName());
-                $url = strtolower($url);
-                $product->setUrlKey($url);
-                echo 'New URL Key: ' . $url . PHP_EOL;
-            }
-            $oldWebsiteIds = $product->getWebsiteIds();
-            if ( $oldWebsiteIds !== $websiteIds)
-            {
-                $product->setWebsiteIds($websiteIds);
-                $product->save();
-                sleep(rand(1, 3));
-            }
-            $productList = null;
-            //die();
+            $product->save();
         } catch (Exception $e) {
             Mage::log($e->getMessage(), null, 'jobs.log');
         }
+        sleep(rand(1, 3));
     }
 }
