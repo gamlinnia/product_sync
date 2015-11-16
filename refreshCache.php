95335ed71ad3471e232b238de5925b5317183558
gamlinnia <intppp01>
2015-11-16 22:00:33 +0800

    refresh caches

--- refreshCache.php
+++ refreshCache.php
@@ -5,9 +5,16 @@ $config = json_decode(file_get_contents('config.json'), true);
 require_once '../' . $config['magentoDir'] . 'app/Mage.php';
 require_once 'functions.php';
 /* if use admin, then websiteId will get 0 */
-Mage::app('default');
+Mage::app('admin');
 
 foreach (array('block_html','collections','fpc') as $type) {
     Mage::app()->getCacheInstance()->cleanType($type);
     Mage::dispatchEvent('adminhtml_cache_refresh_type', array('type' => $type));
+}
+
+$indexCollection = Mage::getModel('index/process')->getCollection();
+foreach ($indexCollection as $type => $index) {
+    Mage::log('Rebuild indexes:' . $index->getIndexerCode(), null, 'jobs.log');
+    /* @var $index Mage_Index_Model_Process */
+    $index->reindexAll();
 }
