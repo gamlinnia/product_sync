5163291258aed7ad70827833d84fc2a4574c0ba6
Li Liu <Li.L.Liu@newegg.com>
2015-11-25 14:20:45 +0800

    debug write to db

--- functions.php
+++ functions.php
@@ -597,7 +597,7 @@ function uploadAndDeleteImagesWithPositionAndLabel ($imageObjectList, $valueToFi
 
     foreach ($imageObjectList['delete'] as $key => $imageObject) {
         $gallery = $product->getMediaGalleryImages();
-        var_dump($gallery->load($imageObject['id']));
+        var_dump($gallery->load(10263));
 //        var_dump($imageObject);
         die();
         foreach ($gallery as $image)
