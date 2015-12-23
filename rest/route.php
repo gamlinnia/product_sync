f2f93398babd23a961f123e3a8cd1282cd10866d
Li Liu <Li.L.Liu@newegg.com>
2015-12-23 17:17:06 +0800

    writeReviewToLocal

--- rest/route.php
+++ rest/route.php
@@ -43,8 +43,8 @@ $app->post('/api/writeReviewToLocal', function () {
     $productObject = getProductObject($input['product']['sku'], 'sku');
     $entity_id = $productObject->getId();
     $customerId = createCustomerNotExist($input['customer']);
+    file_put_contents('review.log', 'customer id: ' . $customerId . PHP_EOL);
     createReviewAndRating($input['review'], $input['rating'], $entity_id, $customerId);
-    file_put_contents('review.log', json_encode($app->request()->headers()->all()));
     file_put_contents('review.log', json_encode($input), FILE_APPEND);
     echo json_encode($input);
 });
