4c7c97b72926b9357d183a22caf4e1e0d8fd07e4
th98 <tim.h.huang@newegg.com>
2015-12-28 16:42:34 +0800

    add /api/writeContactusFormToLocal

--- rest/route.php
+++ rest/route.php
@@ -53,6 +53,7 @@ $app->post('/api/writeContactusFormToLocal', function () {
     global $input;
     global $app;
     $headers = $app->request()->headers();
+    file_put_contents('contactus.log', $input);
     if (!isset($headers['Token']) || $headers['Token'] != 'rosewill') {
         echo json_encode(array(
             'message' => 'auth error.'
