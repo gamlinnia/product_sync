c83649d721946e5c1825420efaed955c77d3ae5e
Li Liu <Li.L.Liu@newegg.com>
2016-03-03 16:27:22 +0800

    get product description with warning message

---
+++ checkDescriptionWithWarningMessage.php
@@ -0,0 +1,19 @@
+#!/usr/bin/php -q
+<?php
+
+if (!file_exists('config.json')) {
+    echo 'config.json is not exist.';
+}
+$config = json_decode(file_get_contents('config.json'), true);
+require_once '../' . $config['magentoDir'] . 'app/Mage.php';
+require_once 'functions.php';
+Mage::app('admin');
+
+$collection = Mage::getModel('catalog/product')->getCollection()->addFieldToFilter('description', 'This product contains a chemical known to the State of California to cause cancer');
+
+
+foreach ($collection as $product) {
+
+}
+
+echo $collection->count();
