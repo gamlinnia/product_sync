34e60a06397ebb0b8a477d852b6eebb3d44db136
Li Liu <Li.L.Liu@newegg.com>
2016-03-07 13:40:27 +0800

    crawl warning of each product.

--- checkDescriptionWithWarningMessage.php
+++ checkDescriptionWithWarningMessage.php
@@ -9,16 +9,66 @@ require_once '../' . $config['magentoDir'] . 'app/Mage.php';
 require_once 'functions.php';
 Mage::app('admin');
 
-$collection = Mage::getModel('catalog/product')->getCollection()->addAttributeToSelect('*');
+$productCollection = Mage::getModel('catalog/product')->getCollection()->addAttributeToSelect('*');
 /*$collection->addAttributeToFilter('description', array(
     array('like' => '% '. 'This product contains a chemical known to the State of California to cause cancer' .' %')
 ));*/
 
-foreach ($collection as $product) {
-    if ($product->getWarning()) {
-        echo $product->getSku() . PHP_EOL;
-        echo $product->getWarning() . PHP_EOL;
+/*channels array*/
+$channels = array(
+    'newegg' => 'http://www.newegg.com/Product/Product.aspx?Item=',
+);
+
+/*foreach channel*/
+foreach($channels as $channel => $url) {
+    /*each excel for each channel */
+    $arrayToExcel = array();
+    /*foreach product*/
+    foreach($productCollection as $eachProduct){
+        if ($eachProduct->getWarning()) {
+            $sku = $eachProduct->getSku();
+            $entity_id = $eachProduct->getId();
+            $productName = $eachProduct->getName();
+            $modelNumber = $eachProduct->getModelNumber();
+            $categoryIds = $eachProduct->getCategoryIds();
+            $attribute_set_id = $eachProduct->getAttributeSetId();
+            $attrInfo = attributeSetNameAndId('attributeSetId', $attribute_set_id);
+            echo 'SKU: ' . $sku . PHP_EOL;
+            echo 'ID: ' . $entity_id . PHP_EOL;
+            echo $eachProduct->getWarning() . PHP_EOL;
+            $arrayToExcel = array(
+                'sku' => $sku,
+                'categorys' => $categoryIds,
+                '$attribute_set_id' => $attrInfo['name']
+            );
+        }
     }
+    /*export all reviews with 1 or 2 rate to excel by channel*/
+//    if(!empty($arrayToExcel)) {
+//        $now = date('Y-m-d');
+//        $fileName = $channel . '_' . $now . '.xls';
+//        $sheetName = 'Sheet 1';
+//        /*push file into fileList*/
+//        $fileList[] = $fileName;
+//        exportArrayToXlsx($arrayToExcel, array(
+//            "filename" => $fileName,
+//            "title" => $sheetName
+//        ));
+//    }
 }
 
+Zend_Debug::dump($arrayToExcel);
 
+function getHylinkLinkOfEachProduct ($channel, $sku) {
+    /* need to include ganon.php */
+    switch ($channel) {
+        case 'newegg' :
+            $html = file_get_dom('http://www.newegg.com/Product/Product.aspx?Item=' . $sku);
+            if(!empty($html)) {
+                foreach ($html('#MfrContact') as $element) {
+                    return $element->getPlainText();
+                }
+            }
+            break;
+    }
+}
