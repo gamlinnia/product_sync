1d7eab3237c58d11bc19ffa11543299479e06065
Li Liu <Li.L.Liu@newegg.com>
2016-03-16 15:32:13 +0800

    crawl review of each product.

--- functions.php
+++ functions.php
@@ -1559,7 +1559,7 @@ function getLatestChannelsProductReviews ($channel, $sku, $channelsinfo) {
             }
             if (isset($channelsinfo['channel_sku']['Amazon.com']) && !empty($channelsinfo['channel_sku']['Amazon.com'])) {
                 var_dump($channelsinfo['channel_sku']);
-                $url = 'http://www.amazon.com/product-reviews/' . $channelsinfo['channel_sku'][$channel] . '/ref=cm_cr_pr_viewopt_srt?ie=UTF8&showViewpoints=1&sortBy=recent&pageNumber=1';
+                $url = 'http://www.amazon.com/product-reviews/' . $channelsinfo['channel_sku']['Amazon.com'] . '/ref=cm_cr_pr_viewopt_srt?ie=UTF8&showViewpoints=1&sortBy=recent&pageNumber=1';
             }
             echo $url . PHP_EOL;
             $html = file_get_dom($url);
