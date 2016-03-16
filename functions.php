b206f9cf0f2d947a317d980774e255b218465e64
Li Liu <Li.L.Liu@newegg.com>
2016-03-16 15:13:14 +0800

    crawl warning of each product. add debug parameter

--- functions.php
+++ functions.php
@@ -1560,6 +1560,7 @@ function getLatestChannelsProductReviews ($channel, $sku, $channelsinfo) {
             if (isset($channelsinfo['channel_sku']) || !empty($channelsinfo['channel_sku'])) {
                 $url = 'http://www.amazon.com/product-reviews/' . $channelsinfo['channel_sku'] . '/ref=cm_cr_pr_viewopt_srt?ie=UTF8&showViewpoints=1&sortBy=recent&pageNumber=1';
             }
+            echo $url . PHP_EOL;
             $html = file_get_dom($url);
             $data = array();
             foreach ($html('#cm_cr-review_list > .a-section') as $index => $element) {
