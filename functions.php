f528b0080d54ae2ec054f9cedad2e7dd7d9192a6
Li Liu <Li.L.Liu@newegg.com>
2016-03-18 17:10:27 +0800

    crawl review of each product from wayfair

--- functions.php
+++ functions.php
@@ -1569,7 +1569,7 @@ function getLatestChannelsProductReviews ($channel, $sku, $channelsinfo) {
             if (isset($channelsinfo['channel_sku']['Wayfair.com']) && !empty($channelsinfo['channel_sku']['Wayfair.com'])) {
                 $url = 'http://www.wayfair.com/a/product_review_page/get_update_reviews_json?_format=json&page_number=1&sort_order=date_desc&filter_rating=&filter_tag=&item_per_page=10&product_sku=' . $channelsinfo[$attr]['Wayfair.com'];
             }
-            $jsonContent = json_decode(file_get_contents($url), true);
+            $jsonContent = json_decode(trim(file_get_contents($url)), true);
             echo json_encode($jsonContent['reviews']) . PHP_EOL;
             break;
         case 'homedepot' :
