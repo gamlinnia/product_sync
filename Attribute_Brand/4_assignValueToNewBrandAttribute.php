31192c3593b854efaa8f72c690be2730b1c0b5dd
th98 <tim.h.huang@newegg.com>
2016-04-01 16:38:34 +0800

    assign attribute to attribute set

--- Attribute_Brand/4_assignValueToNewBrandAttribute.php
+++ Attribute_Brand/4_assignValueToNewBrandAttribute.php
@@ -8,19 +8,23 @@ require_once '../../' . $config['magentoDir'] . 'app/Mage.php';
 require_once '../functions.php';
 Mage::app('admin');
 
+$new_attribute_code = 'brand';
+
 $new_attribute_options = getAttributeOptions('attributeName', 'brand');
+
 var_dump($new_attribute_options);
-//die();
+
 foreach($new_attribute_options['options'] as $option){
     if(strtolower($option['label']) == strtolower('rosewill')){
         $new_attribute_value = $option['value'];
     }
 }
 echo $new_attribute_value . PHP_EOL;
-die();
 
 $productCollection = Mage::getModel('catalog/product')->getCollection();
 foreach($productCollection as $each) {
+    $product = Mage::getModel('catalog/product')->load($each->getId());
+    echo "Product ID: " . $product->getId();
     try {
         $product->setData($new_attribute_code, $new_attribute_value);
         $product->save();
