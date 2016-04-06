e813e12cb1202fa3857b803bd08a7e06cab2b53f
th98 <tim.h.huang@newegg.com>
2016-04-06 10:36:30 +0800

    assign attribute to attribute set

--- Attribute_Warranty/1_assignWarrantyToAllAttributeSet.php
+++ Attribute_Warranty/1_assignWarrantyToAllAttributeSet.php
@@ -18,24 +18,23 @@ foreach($attributesNeedToAssign as $regularEx => $eachNeedToAssign) {
     $attributeSetCollection = Mage::getResourceModel('eav/entity_attribute_set_collection');
 
     foreach ($attributeSetCollection as $each) {
-        $attributes = Mage::getModel('catalog/product_attribute_api')->items($each->getId());
         $attribute_set_name = $each->getAttributeSetName();
         echo $attribute_set_name . PHP_EOL;
+        $attributes = Mage::getModel('catalog/product_attribute_api')->items($each->getId());
         foreach ($attributes as $eachAttr) {
 //            echo "    " . $eachAttr['code'] . PHP_EOL;
             preg_match('/' . $regularEx . '/', $eachAttr['code'], $matchWarranty);
             if (count($matchWarranty) >= 1) {
                 echo "    " . $eachAttr['code'] . PHP_EOL;
-//                echo "=================================================Match=========================================================" . PHP_EOL;
-
-                $attributeSetId = $model->getAttributeSetId('catalog_product', $attribute_set_name);
+                $attributeSetId = $each->getAttributeSetId();
+                //$attributeSetId = $model->getAttributeSetId('catalog_product', $attribute_set_name);
                 $attributeGroupDataArray = $model->getAttributeGroup('catalog_product', $attributeSetId, $attribute_set_name);
                 if(!debug){
                     $model->addAttributeToSet('catalog_product', $attributeSetId, $attributeGroupDataArray["attribute_group_id"], $attributeId);
                 }
+                echo $attributeSetId . PHP_EOL;
                 var_dump($attributeGroupDataArray);
                 echo $attributeId . PHP_EOL;
-                echo $attributeSetId . PHP_EOL;
                 die();
             }
         }
