c8f66a37cf50544438301631744c732e2e78a28e
th98 <tim.h.huang@newegg.com>
2016-04-06 13:54:18 +0800

    assign attribute to attribute set

--- Attribute_Model/1_removeWarrantyAttribute.php
+++ Attribute_Model/1_removeWarrantyAttribute.php
@@ -18,23 +18,19 @@ $attributeCollection = Mage::getModel('catalog/resource_eav_attribute')->getColl
 $prepareToRemove = array();
 
 foreach ($attributeCollection as $eachAttr) {
-    preg_match('/_model/', $eachAttr['code'], $matchWarranty);
+    preg_match('/_model/', $eachAttr->getCode(), $matchWarranty);
     if (count($matchWarranty) >= 1) {
         $prepareToRemove[] = array(
-            'attribute_set_id' => $each->getId(),
-            'attribute_set_name' => $each->getAttributeSetName(),
-            'attribute_code' => $eachAttr['code'],
-            'attribute_id' => $eachAttr['attribute_id']
+            'attribute_code' => $eachAttr->getCode(),
+            'attribute_id' => $eachAttr->getId()
         );
         echo "=============================================================================" . PHP_EOL;
-        echo "Attrbiute set name: " . $each->getAttributeSetName() . PHP_EOL;
-        echo "Attrbiute set ID: " . $each->getId() . PHP_EOL;
-        echo "    Attrbiute name: " . $eachAttr['code'] . PHP_EOL;
-        echo "    Attrbiute ID: " . $eachAttr['attribute_id'] . PHP_EOL;
+        echo "    Attrbiute name: " . $eachAttr->getCode() . PHP_EOL;
+        echo "    Attrbiute ID: " . $eachAttr->getId() . PHP_EOL;
         if(!$debug) {
             //delete attribute from database
             $setup = Mage::getResourceModel('catalog/setup', 'catalog_setup');
-            $setup->removeAttribute('catalog_product', $eachAttr['code']);
+            $setup->removeAttribute('catalog_product', $eachAttr->getCode());
         }
     }
 }
