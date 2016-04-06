345a24f5c07dd2f656d36bd34d20975a996023ef
th98 <tim.h.huang@newegg.com>
2016-04-06 13:11:45 +0800

    assign attribute to attribute set

--- Attribute_Warranty/3_removeWarrantyAttribute.php
+++ Attribute_Warranty/3_removeWarrantyAttribute.php
@@ -8,6 +8,13 @@ require_once '../../' . $config['magentoDir'] . 'app/Mage.php';
 require_once '../functions.php';
 Mage::app('admin');
 
+$debug = false;
+if (in_array('debug', $argv)) {
+    $debug = true;
+}
+
+$attributesNeedToRemove = array('_manufacturer_warranty_p', '_manufacturer_warranty_l');
+
 $attributeSetCollection = Mage::getResourceModel('eav/entity_attribute_set_collection');
 
 $prepareToRemove = array();
@@ -15,26 +22,30 @@ foreach($attributeSetCollection as $each) {
     $attributes = Mage::getModel('catalog/product_attribute_api')->items($each->getId());
     $attributeCode = array();
     foreach ($attributes as $eachAttr) {
-        preg_match('/_brand$/', $eachAttr['code'], $matchBrand);
-        if (count($matchBrand) >= 1) {
-            $prepareToRemove[] = array(
-                'attribute_set_id' => $each->getId(),
-                'attribute_set_name' => $each->getAttributeSetName(),
-                'attribute_code' => $eachAttr['code'],
-                'attribute_id' => $eachAttr['attribute_id']
-            );
-            echo "=============================================================================" . PHP_EOL;
-            echo "Attrbiute set name: " . $each->getAttributeSetName() . PHP_EOL;
-            echo "Attrbiute set ID: " . $each->getId() . PHP_EOL;
-            echo "Attrbiute name: " . $eachAttr['code'] . PHP_EOL;
-            echo "Attrbiute ID: " . $eachAttr['attribute_id'] . PHP_EOL;
-            //delete attribute from database
-            $setup = Mage::getResourceModel('catalog/setup','catalog_setup');
-            $setup->removeAttribute('catalog_product', $eachAttr['code']);
+        foreach($attributesNeedToRemove as $eachAttrNeedToRemove) {
+            preg_match('/' . $eachAttrNeedToRemove .'/', $eachAttr['code'], $matchWarranty);
+            if (count($matchWarranty) >= 1) {
+                $prepareToRemove[] = array(
+                    'attribute_set_id' => $each->getId(),
+                    'attribute_set_name' => $each->getAttributeSetName(),
+                    'attribute_code' => $eachAttr['code'],
+                    'attribute_id' => $eachAttr['attribute_id']
+                );
+                echo "=============================================================================" . PHP_EOL;
+                echo "Attrbiute set name: " . $each->getAttributeSetName() . PHP_EOL;
+                echo "Attrbiute set ID: " . $each->getId() . PHP_EOL;
+                echo "Attrbiute name: " . $eachAttr['code'] . PHP_EOL;
+                echo "Attrbiute ID: " . $eachAttr['attribute_id'] . PHP_EOL;
+                if(!$debug) {
+                    //delete attribute from database
+                    $setup = Mage::getResourceModel('catalog/setup', 'catalog_setup');
+                    $setup->removeAttribute('catalog_product', $eachAttr['code']);
+                }
+            }
         }
     }
 
 }
 
-var_dump($prepareToRemove);
-file_put_contents('remove_brand_attribute.txt', json_encode($prepareToRemove));
+//var_dump($prepareToRemove);
+//file_put_contents('remove_warranty_attribute.txt', json_encode($prepareToRemove));
