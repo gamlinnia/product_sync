d08e75763f6c82d874f0d5c7445eb16a61109e28
th98 <tim.h.huang@newegg.com>
2016-04-07 16:08:47 +0800

    attribute features

--- Attribute_Brand/5_moveBrandAttributeToGeneralGroup.php
+++ Attribute_Brand/5_moveBrandAttributeToGeneralGroup.php
@@ -21,6 +21,7 @@ $attributeCollection = Mage::getResourceModel('eav/entity_attribute_collection')
 foreach($attributeSetCollection as $each){
     $attributes = Mage::getModel('catalog/product_attribute_api')->items($each->getId());
     $attributeSetName = $each->getAttributeSetName();
+    echo $attributeSetName . PHP_EOL;
     foreach($attributes as $eachAttr){
         preg_match('/brand/', $eachAttr['code'], $matchBrand);
         if(count($matchBrand) >= 1){
