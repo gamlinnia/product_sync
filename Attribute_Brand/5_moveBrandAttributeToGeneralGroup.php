92a7b5ed54e5a0ee48a5911b78fcbcd1a4010cd8
th98 <tim.h.huang@newegg.com>
2016-04-07 16:06:48 +0800

    attribute features

---
+++ Attribute_Brand/5_moveBrandAttributeToGeneralGroup.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * for color attribute now
+ *
+ * */
+$config = json_decode(file_get_contents('../config.json'), true);
+require_once '../../' . $config['magentoDir'] . 'app/Mage.php';
+require_once '../functions.php';
+Mage::app('admin');
+
+$debug = false;
+if (in_array('debug', $argv)) {
+    $debug = true;
+}
+
+$attributeName = 'brand';
+$groupName = 'General';
+
+$attributeCollection = Mage::getResourceModel('eav/entity_attribute_collection');
+
+foreach($attributeSetCollection as $each){
+    $attributes = Mage::getModel('catalog/product_attribute_api')->items($each->getId());
+    $attributeSetName = $each->getAttributeSetName();
+    foreach($attributes as $eachAttr){
+        preg_match('/brand/', $eachAttr['code'], $matchBrand);
+        if(count($matchBrand) >= 1){
+            moveAttributeToGroup($attributeName, $attributeSetName, $groupName);
+        }
+    }
+}
