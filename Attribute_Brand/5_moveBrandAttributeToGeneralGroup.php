915112835958a20d8c65a16ec25622e0384cf900
th98 <tim.h.huang@newegg.com>
2016-04-07 16:14:36 +0800

    attribute features

--- Attribute_Brand/5_moveBrandAttributeToGeneralGroup.php
+++ Attribute_Brand/5_moveBrandAttributeToGeneralGroup.php
@@ -15,18 +15,20 @@ if (in_array('debug', $argv)) {
 
 $attributeName = 'brand';
 $groupName = 'General';
-
+$excludeAttributeSet = array('Default');
 $attributeSetCollection = Mage::getResourceModel('eav/entity_attribute_set_collection');
 
 foreach($attributeSetCollection as $each){
     $attributes = Mage::getModel('catalog/product_attribute_api')->items($each->getId());
     $attributeSetName = $each->getAttributeSetName();
     echo $attributeSetName . PHP_EOL;
-    foreach($attributes as $eachAttr){
-        echo "    " . $eachAttr['code'] . PHP_EOL;
-        preg_match('/brand/', $eachAttr['code'], $matchBrand);
-        if(count($matchBrand) >= 1){
-            moveAttributeToGroup($attributeName, $attributeSetName, $groupName);
+    if(!in_array($attributeSetName, $excludeAttributeSet)) {
+        foreach ($attributes as $eachAttr) {
+            echo "    " . $eachAttr['code'] . PHP_EOL;
+            preg_match('/brand/', $eachAttr['code'], $matchBrand);
+            if (count($matchBrand) >= 1) {
+                moveAttributeToGroup($attributeName, $attributeSetName, $groupName);
+            }
         }
     }
 }
