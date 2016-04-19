fa0698510a32c6e17432dc9ee3cec9d0adcc37c9
Li Liu <Li.L.Liu@newegg.com>
2016-04-19 13:15:29 +0800

    modifyMappingTable

--- importNewProduct.php
+++ importNewProduct.php
@@ -10,12 +10,16 @@ require_once 'functions.php';
 Mage::app('admin');
 
 if (!isset($argv[1])) {
-    preg_match('/[\d]{2}-[\d]{3}-[\d]{3}/', $argv[1] , $match);
-    if (count($match) < 1) {
-        echo 'Model number is not specified.' . PHP_EOL;
-        return;
-    }
+    echo 'Model number is not specified.' . PHP_EOL;
+    return;
 }
+
+preg_match('/[\d]{2}-[\d]{3}-[\d]{3}/', $argv[1] , $match);
+if (count($match) < 1) {
+    echo 'Model number is not specified.' . PHP_EOL;
+    return;
+}
+$sku = $match[0];
 
 /* save product json to local files in dev environment. */
 $dir = './rest/productJson/';
@@ -25,4 +29,7 @@ if (!file_get_contents($dir . 'mappingAttrs.json')) {
 }
 
 
-$mapTable = json_encode(file_get_contents($dir . 'mappingAttrs.json'));
+$mapTable = json_decode(file_get_contents($dir . 'mappingAttrs.json'));
+var_dump($mapTable);
+$productJson = json_decode(file_get_contents($dir . $sku));
+var_dump($productJson);
