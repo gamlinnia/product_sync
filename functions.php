244e2beacc17e341adc5bd1c58f376d4d8ff9de2
Li Liu <Li.L.Liu@newegg.com>
2016-06-23 11:06:52 +0800

    uploadAndDeleteImagesWithPositionAndLabel

--- functions.php
+++ functions.php
@@ -2696,16 +2696,18 @@ function uploadProductImageByNewModule ($productModel, $imgUrl, $position, $labe
         echo 'media id: ' . $mediaId . PHP_EOL;
         $fileValue = $eachMedia->getValue();
         echo 'file value: ' . $fileValue . PHP_EOL;
-        $mediagalleryvalue = Mage::getModel('coreproductmediagallery/mediagalleryvalue')
-            ->getCollection()
-            ->addFieldToFilter('value_id', $mediaId)
-            ->addFieldToFilter('store_id', 0);
-        foreach ($mediagalleryvalue as $eachMediaValue) {
-            $eachMediaValue->setData('label', $label)
-            ->setData('position', $position);
-            $eachMediaValue->save();
+        $storeIds = getAllStoreIds();
+        foreach ($storeIds as $eachStoreId) {
+            $mediagalleryvalue = Mage::getModel('coreproductmediagallery/mediagalleryvalue')
+                ->getCollection()
+                ->addFieldToFilter('value_id', $mediaId)
+                ->addFieldToFilter('store_id', $eachStoreId);
+            foreach ($mediagalleryvalue as $eachMediaValue) {
+                $eachMediaValue->setData('label', $label)
+                    ->setData('position', $position);
+                $eachMediaValue->save();
+            }
         }
-
     }
 
 }
