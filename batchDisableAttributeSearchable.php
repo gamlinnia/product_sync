9eab20ca8e3233038bcb8c7f85a329d98a80f135
Li Liu <Li.L.Liu@newegg.com>
2016-07-01 14:41:04 +0800

    batchDisableAttributeSearchable

--- batchDisableAttributeSearchable.php
+++ batchDisableAttributeSearchable.php
@@ -13,7 +13,7 @@ Mage::app('admin');
 $collection = Mage::getModel('eav/entity_attribute')->getCollection()->addFieldToFilter('entity_type_id', 4);
 foreach ($collection as $eachAttribute) {
     $attributeModel = Mage::getModel('eav/entity_attribute')->load($eachAttribute->getId());
-    if ($attributeModel->getData('is_searchable') == 1) {
+    if ($attributeModel->getData('is_searchable') == 1 || $attributeModel->getData('is_visible_in_advanced_search') == 1) {
         Zend_Debug::dump($attributeModel->getData());
         $attributeModel
             ->setData('is_searchable', 0)
