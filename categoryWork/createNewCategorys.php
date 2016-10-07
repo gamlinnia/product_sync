6631d5a0729245c6774746f9f67f61894c8a7787
gamlinnia <li.l.liu@newegg.com>
2016-10-07 16:22:38 +0800

    create batch new categorys

---
+++ categoryWork/createNewCategorys.php
@@ -0,0 +1,103 @@
+<?php
+
+$categorysAddList = array(
+    'Headsets & Speakers' => array(
+        'Headsets', 'Speakers'
+    ),
+    'Case' => array(
+        'Case'
+    ),
+    'Power Supply' => array(
+        'Power Supplies'
+    ),
+    'Mice' => array(
+        'Mice  & Accessories'
+    ),
+    'Keyboards' => array(
+        'Keyboards & Accessories'
+    ),
+    'Computer Accessories' => array(
+        'Cables', 'PC Tools & Accessories', 'USB Hubs & Accessories', 'Hard Drive Docking Stations & External Enclosures', 'Card Readers',
+        'Monitor Mounts', 'Projector Mounts', 'Power Strips & Surge Protectors'
+    ),
+    'Electronics & Accessories' => array(
+        'Laptop Backpack & Case', 'Laptop AC Adapter', 'Cellphone & Tablet Mounts', 'Battery & Chargers'
+    ),
+    'Computer Components' => array(
+        'Fans & Accessories', 'SSD & HDD Accessories', 'Coolers', 'Add-on Cards', 'Internal Card Readers and Hubs', 'HDD Enclosures',
+        'Hard Drive Controllers & RAID Cards'
+    ),
+    'Networking' => array(
+        'Wireless Accessories', 'Network Interface Cards', 'Network Switches', 'Modems', 'Wireless Adapters', 'Wireless Routers', 'Network Antennas'
+    ),
+    'Server Systems & Components' => array(
+        'Server Accessories', 'Server Chassis', 'RAID - Sub Systems'
+    ),
+    'Surveillance Cameras' => array(
+        'IP & Network Cameras', 'CCTV & Analog Cameras', 'Video Monitoring Kits'
+    ),
+    'Printer & Office Accessories' => array(
+        'Ink & Toner', 'Office Furniture', 'Office Electronics'
+    ),
+    'Living Room & Other Appliances' => array(
+        'TV Antennas & Accessories', 'TV Brackets', 'Fans', 'Heaters', 'Humidifiers', 'Tool & Electrical Accessories'
+    ),
+    'Kitchen Appliances' => array(
+        'Bread Makers', 'Induction Cooktops', 'Rice Cookers', 'Cutlery', 'Air Fryers', 'Ice Cream Makers', 'Juicers & Extractors',
+        'Kitchen Scales', 'Popcorn Poppers', 'Thermo Pots', 'Toaster Ovens', 'Ice Makers', 'Electric Kettles', 'Food Dehydrators',
+        'Food Steamers', 'Pressure Cookers'
+    )
+);
+
+$categorysAddList = array(
+    'Headsets & Speakers' => array(
+        'Headsets', 'Speakers'
+    )
+);
+
+
+foreach ($categorysAddList as $mainCategoryName => $subCategoryArray) {
+    echo 'deal with main category: ' . $mainCategoryName . PHP_EOL;
+    $mainCategoryId = createCategory($mainCategoryName, null);
+
+    if ($mainCategoryId) {
+        foreach ($subCategoryArray as $subCategoryName) {
+            echo 'deal with sub category: ' . $subCategoryName . PHP_EOL;
+            $subCategoryId = createCategory($subCategoryName, $mainCategoryId);
+
+        }
+    }
+}
+
+function createCategory ($name, $parentId = null, $enabled = 0) {
+    $categoryCollection = Mage::getModel( 'catalog/category' )->getCollection()
+        ->addAttributeToFilter('name', $name);
+    if ($categoryCollection->count() < 1) {
+        $category = Mage::getModel('catalog/category');
+        $category->setStoreId(0);
+        $category->setName($name); // The name of the category
+        $category->setUrlKey(strtolower(str_replace(' ', '-', $name))); // The category's URL identifier
+        $category->setIsActive($enabled); // Is it enabled?
+        $category->setIsAnchor(0);
+        $category->setDisplayMode('PRODUCTS');
+        $category->setPath('1/2/' . $parentId); // Important you get this right.
+        $category->setPageTitle($name);
+        $category->save();
+
+        $mainCategoryId = $category->getId();
+
+        if ($parentId) {
+            Mage::getModel('catalog/category')->load($mainCategoryId)
+                ->setPath('1/2/' . $parentId . '/' . $mainCategoryId)
+                ->save();
+        } else {
+            Mage::getModel('catalog/category')->load($mainCategoryId)
+                ->setPath('1/2/' . $mainCategoryId)
+                ->save();
+        }
+
+        return $mainCategoryId;
+    } else {
+        return null;
+    }
+}
