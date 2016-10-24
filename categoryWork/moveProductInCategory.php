0133250236d6e58e6347d83bf1c53d220c1d07f5
gamlinnia <li.l.liu@newegg.com>
2016-10-24 11:36:27 +0800

    move old category products to new category

--- categoryWork/moveProductInCategory.php
+++ categoryWork/moveProductInCategory.php
@@ -146,7 +146,7 @@ $new_category_mapping_table = array(
 
 foreach ($new_category_mapping_table as $category_name_to_be_mapped => $map_to_category) {
     $category = getCategoryByName($category_name_to_be_mapped);
-    if (!$category->getId()) {
+    if (empty($category)) {
         echo 'category name: ' . $category_name_to_be_mapped . ' map to nothing' . PHP_EOL;
         exit(0);
     }
