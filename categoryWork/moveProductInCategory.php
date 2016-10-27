00a515bbf8bce6e52baa5cb7d0bdb8ce7be41304
gamlinnia <li.l.liu@newegg.com>
2016-10-27 17:19:37 +0800

    move old category products to new category

--- categoryWork/moveProductInCategory.php
+++ categoryWork/moveProductInCategory.php
@@ -147,7 +147,7 @@ $new_category_mapping_table = array(
 );
 
 $new_category_mapping_table = array(
-    'Tool & Electrical Accessories' => 'Tools & Electrical Accessories'
+    'Gaming Cases' => 'Case'
 );
 
 $noMapCategorys = array();
