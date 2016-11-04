f9a5fc9505549dbb730d69910a0c90c496986beb
gamlinnia <li.l.liu@newegg.com>
2016-11-04 11:15:33 +0800

    attribute work

--- attributeWork/get_attribute_list.php
+++ attributeWork/get_attribute_list.php
@@ -129,7 +129,9 @@ function main() {
                 $attr = Mage::getModel('eav/entity_attribute')->load(
                     $_attr->getId()
                 );
+                Zend_Debug::dump($attr->getData('attribute_code'));
                 Zend_Debug::dump($attr->getData('frontend_label'));
+                Zend_Debug::dump($attr->getData('frontend_input'));
             }
             echo 'similar attr count: ' . $attr_collection->count() . PHP_EOL;
 
