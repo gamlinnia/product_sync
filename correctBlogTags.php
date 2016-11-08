cde612ce2714548d9b5929d9199dc089199c26e2
m05542322 <m05542322@gmail.com>
2016-11-08 17:20:27 +0800

    fix correctBlogTags.php

--- correctBlogTags.php
+++ correctBlogTags.php
@@ -12,10 +12,19 @@ $collection = Mage::getModel('blog/post')->getCollection();
 
 foreach($collection as $each) {
     $model = Mage::getModel('blog/post')->load($each->getId());
-    $tags = $each->getTags();
+    $tags = $model->getTags();
     $tags = explode(',', $tags);
     $tags = array_map('trim', $tags);
     $tags = implode(',', $tags);
+    $cat = $model->getCatId();
+    var_dump($model->getTitle());
+    if(empty($cat)) {
+        echo "1. News / 2. Editorial Reviews / 3. Blogs" . PHP_EOL;
+        $cat_id = trim(fgets(STDIN));
+        $cat = array($cat_id);
+    }
     $model->setTags($tags)
+        ->setCats($cat)
         ->save();
 }
+
