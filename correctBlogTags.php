853411399cbd7a1f694f7ee5ae6ad874f66cde85
m05542322 <m05542322@gmail.com>
2016-11-08 17:24:58 +0800

    fix correctBlogTags.php

--- correctBlogTags.php
+++ correctBlogTags.php
@@ -3,7 +3,6 @@ if (!file_exists('config.json')) {
     echo 'config.json is not exist.';
 }
 $config = json_decode(file_get_contents('config.json'), true);
-#require_once '../enterprise/public_html/app/Mage.php';
 require_once '../' . $config['magentoDir'] . 'app/Mage.php';
 require_once 'functions.php';
 Mage::app('admin');
