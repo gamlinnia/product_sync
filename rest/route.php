f2df10d78f9839b6c5c784f9122256359bef5c9d
m05542322 <m05542322@gmail.com>
2016-11-15 11:31:48 +0800

    fix bug of syncDownloadableFileList

--- rest/route.php
+++ rest/route.php
@@ -364,7 +364,10 @@ $app->post('/api/syncDownloadableFileList', function() {
     //input is ok
     $remoteFileList = $input;
     $localFileList = getDownloadableFileList();
-    echo json_encode(array_keys($localFileList));
+
+    $remoteFileListKeys = array_keys($remoteFileList);
+    $localFileListKeys = array_keys($localFileList);
+    echo json_encode(array(array_diff($remoteFileListKeys, $localFileListKeys), array_diff($localFileListKeys, $remoteFileListKeys)));
 //    $localNeedToAdd = arrayDiff($localFileList, $remoteFileList);
 //    $remoteNeedToAdd = arrayDiff($remoteFileList, $localFileList);
 //    echo json_encode($remoteNeedToAdd);
