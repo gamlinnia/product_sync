c891dfda69e6d90598e265639ae71ce5ad20516f
m05542322 <m05542322@gmail.com>
2017-01-18 16:48:12 +0800

    add modify function to attribute revise

--- attributeWork/get_attribute_list.php
+++ attributeWork/get_attribute_list.php
@@ -194,13 +194,13 @@ function main() {
             echo 'to be added option list: ' . implode(' / ', $toAddArray) . ' count: ' . count($toAddArray) . PHP_EOL . PHP_EOL;
 
             if (count($toAddArray) > 0) {
-                $prompt = strtolower(promptMessageForInput('add below options ?(Y/n) or modify these options(M/m): ' . implode(' / ', $toAddArray) ));
+                $prompt = strtolower(promptMessageForInput('add below options ?(Y/n) or modify these options(M/m): ' . implode(' , ', $toAddArray) ));
                 if ($prompt == 'y'|| $prompt == 'yes') {
                     setAttributeOptions($new_attr_id, $toAddArray);
                 }
                 elseif ($prompt == 'm') {
-                    $prompt = promptMessageForInput('enter the string of all options(separate by "/")');
-                    $newOptionsArray = explode('/', $prompt);
+                    $prompt = promptMessageForInput('enter the string of all options(separate by ",")');
+                    $newOptionsArray = explode(',', $prompt);
                     $newOptionsArray = array_map('trim', $newOptionsArray);
 //                    Zend_Debug::dump($newOptionsArray);
                     $prompt = strtolower(promptMessageForInput('sure to add these new options above ?(Y/n)'));
